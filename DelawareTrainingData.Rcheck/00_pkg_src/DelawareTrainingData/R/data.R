61ccdce2efe4020b7f58fb2d96398ac7ccdc901e
zrussek <z.russek@gmail.com>
2015-11-13 16:51:22 -0600

    Add files

---
+++ DelawareTrainingData.Rcheck/00_pkg_src/DelawareTrainingData/R/data.R
@@ -0,0 +1,10 @@
+#' deid_dems_single_row_per_student.
+#'
+#' Demographic data of students. One student per row of data.
+#' Short dataset with only 11 students and 11 variables.
+#'
+#' @format A data frame with eleven variables: \code{fake_student_id}, \code{gender},
+#'   \code{race}, \code{race_descr}, \code{migrant_ind}, \code{homeless_ind},
+#'   \code{sped_ind}, \code{sped_code}, \code{sped_descr}, \code{low_income_ind},
+#'    and \code{ell_status}.
+"deid_dems_single_row_per_student"
---
+++ DelawareTrainingData/data_raw/save_data.R
@@ -0,0 +1,16 @@
+deid_dems_single_row_per_student <- read.csv("P:/GitHub/training/DelawareTrainingData/data_raw/deid_dems_single_row_per_student.csv")
+save(deid_dems_single_row_per_student, file = "data/deid_dems_single_row_per_student.rdata")
+
+deid_dems_single_row_per_student_with_grade <- read.csv("P:/GitHub/training/DelawareTrainingData/data_raw/deid_dems_single_row_per_student_with_grade.csv")
+save(deid_dems_single_row_per_student_with_grade, file = "data/deid_dems_single_row_per_student_with_grade.rdata")
+
+deid_dems_single_row_per_year <- read.csv("P:/GitHub/training/DelawareTrainingData/data_raw/deid_dems_single_row_per_year.csv")
+save(deid_dems_single_row_per_year, file = "data/deid_dems_single_row_per_year.rdata")
+
+deid_test_multi_row <- read.csv("P:/GitHub/training/DelawareTrainingData/data_raw/deid_test_multi_row.csv")
+save(deid_test_multi_row, file = "data/deid_test_multi_row.rdata")
+
+deid_test_single_row <- read.csv("P:/GitHub/training/DelawareTrainingData/data_raw/deid_test_single_row.csv")
+save(deid_test_single_row, file = "data/deid_test_single_row.rdata")
+
+
