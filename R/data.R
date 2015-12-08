8982d8e1999ffa7a309dcf84f76efd22cda15120
zrussek <zrussek@EA10.EA.local>
2015-12-08 09:37:03 -0600

    fix

---
+++ R/data.R
@@ -0,0 +1,60 @@
+#' deid_dems_single_row_per_student.
+#'
+#' Demographic data of students. One student per row of data.
+#' Short dataset with only 11 students and 11 variables.
+#'
+#' @format A data frame with eleven variables: \code{fake_student_id}, \code{gender},
+#'   \code{race}, \code{race_descr}, \code{migrant_ind}, \code{homeless_ind},
+#'   \code{sped_ind}, \code{sped_code}, \code{sped_descr}, \code{low_income_ind},
+#'    and \code{ell_status}.
+"deid_dems_single_row_per_student"
+
+#' deid_dems_single_row_per_student_with_grade
+#'
+#' Demographic data of students. One student per row of data.
+#' Short dataset with only 11 students and 12 variables.
+#'
+#' @format A data frame with twelve variables: \code{fake_student_id}, \code{gender},
+#'   \code{race}, \code{race_descr}, \code{migrant_ind}, \code{homeless_ind},
+#'   \code{sped_ind}, \code{sped_code}, \code{sped_descr}, \code{low_income_ind},
+#'    \code{ell_status}, and \code{grade}.
+"deid_dems_single_row_per_student_with_grade"
+
+#' deid_dems_single_row_per_year
+#'
+#' Demographic data of students. One student per row of data.
+#' Dataset has 74 students and 12 variables.
+#'
+#' @format A data frame with twelve variables:  \code{fake_student_id},
+#'  \code{school_year}, \code{gender}, \code{race}, \code{race_descr}, \code{migrant_ind},
+#'  \code{homeless_ind}, \code{sped_ind}, \code{sped_code}, \code{sped_descr},
+#'  \code{low_income_ind}, and \code{ell_status}.
+"deid_dems_single_row_per_year"
+
+#' deid_test_multi_row
+#'
+#' Demographic data of students. One student per row of data.
+#' Dataset has 500 students and 14 variables.
+#'
+#' @format A data frame with twelve variables:  \code{school_year},
+#' \code{fake_student_id}, \code{fake_school_id}, \code{test_name},
+#' \code{test_period}, \code{opportunity_number}, \code{test_grade},
+#' \code{test_content_area}, \code{test_start_date}, \code{test_end_date},
+#' \code{test_sem}, \code{test_scale_score}, \code{test_performance_level}, and \code{test_raw_score}.
+"deid_test_multi_row"
+
+#' deid_test_single_row
+#'
+#' Demographic data of students. One student per row of data.
+#' Dataset has 8 students and 14 variables.
+#'
+#' @format A data frame with twelve variables:  \code{school_year},
+#' \code{fake_student_id}, \code{fake_school_id}, \code{test_name},
+#' \code{test_period}, \code{opportunity_number}, \code{test_grade},
+#' \code{test_content_area}, \code{test_start_date}, \code{test_end_date},
+#' \code{test_sem}, \code{test_scale_score}, \code{test_performance_level}, and \code{test_raw_score}.
+"deid_test_single_row"
+
+
+
+
