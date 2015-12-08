916946f436c781fbc9bf1741a57143d9c53ea54d
zrussek <zrussek@EA10.EA.local>
2015-12-08 11:35:53 -0600

    add documentation for answers

--- R/data.R
+++ R/data.R
@@ -66,4 +66,44 @@
 #' and \code{d_lep_y}.
 "data_ggplot2"
 
+#' answer_2subset
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 2_subset.
+#'
+"answer_2subset"
 
+#' answer_3merge
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 3_merge.
+#'
+"answer_3merge"
+
+#' answer_4recode
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 4_recode.
+#'
+"answer_4recode"
+
+#' answer_5operations
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 5_operations.
+#'
+"answer_5operations"
+
+#' answer_6bygroup
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 6_by_group.
+#'
+"answer_6bygroup"
+
+#' answer_7transpose
+#'
+#' A dataset that can be used to check your answers to the do it yourself section of 7_transpose.
+#'
+"answer_7transpose"
+
+#' answer_9loop
+#'
+#' A variable that can be used to check your answers to the do it yourself section of 9_loop.
+#'
+"answer_9loop"
--- data_raw/save_data.R
+++ data_raw/save_data.R
@@ -1,17 +1,24 @@
-dems_single_row_student <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/deid_dems_single_row_per_student.csv")
+dems_single_row_student <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_student.csv")
 save(dems_single_row_student, file = "data/dems_single_row_student.rdata")
 
-dems_single_row_student_wgrade <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/deid_dems_single_row_per_student_with_grade.csv")
+dems_single_row_student_wgrade <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_student_with_grade.csv")
 save(dems_single_row_student_wgrade, file = "data/dems_single_row_student_wgrade.rdata")
 
-dems_single_row_year <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/deid_dems_single_row_per_year.csv")
+dems_single_row_year <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_year.csv")
 save(dems_single_row_year, file = "data/dems_single_row_year.rdata")
 
-test_multi_row <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/deid_test_multi_row.csv")
+test_multi_row <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_test_multi_row.csv")
 save(test_multi_row, file = "data/test_multi_row.rdata")
 
-test_single_row <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/deid_test_single_row.csv")
+test_single_row <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_test_single_row.csv")
 save(test_single_row, file = "data/test_single_row.rdata")
 
-data_ggplot2 <- read.csv("P:/GitHub/Delaware_Data_Package/data_raw/sample_test_data_ggplot2.csv")
+data_ggplot2 <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/sample_test_data_ggplot2.csv")
 save(data_ggplot2, file = "data/data_ggplot2.rdata")
+
+doit_yourself <- load("P:/GitHub/DelawareDataPackage/data_raw/answers_doit_yourself.Rdata")
+for(i in 1:length(doit_yourself)){
+  working <- get(doit_yourself[i])
+  save(working, file = paste0("data/",doit_yourself[i],".rdata"))
+}
+
