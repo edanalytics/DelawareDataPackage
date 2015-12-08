3a50740e3c82c38699f86d776de7549e4a5d39ab
zrussek <zrussek@EA10.EA.local>
2015-12-08 12:08:19 -0600

    documentation updatae

--- R/data.R
+++ R/data.R
@@ -3,10 +3,6 @@
 #' Demographic data of students. One student per row of data.
 #' Short dataset with only 11 students and 11 variables.
 #'
-#' @format A data frame with eleven variables: \code{fake_student_id}, \code{gender},
-#'   \code{race}, \code{race_descr}, \code{migrant_ind}, \code{homeless_ind},
-#'   \code{sped_ind}, \code{sped_code}, \code{sped_descr}, \code{low_income_ind},
-#'    and \code{ell_status}.
 "dems_single_row_student"
 
 #' dems_single_row_student_wgrade.
@@ -14,10 +10,6 @@
 #' Demographic data of students. One student per row of data.
 #' Short dataset with only 11 students and 12 variables.
 #'
-#' @format A data frame with twelve variables: \code{fake_student_id}, \code{gender},
-#'   \code{race}, \code{race_descr}, \code{migrant_ind}, \code{homeless_ind},
-#'   \code{sped_ind}, \code{sped_code}, \code{sped_descr}, \code{low_income_ind},
-#'    \code{ell_status}, and \code{grade}.
 "dems_single_row_student_wgrade"
 
 #' dems_single_row_year
@@ -25,10 +17,6 @@
 #' Demographic data of students. One student per row of data.
 #' Dataset has 74 students and 12 variables.
 #'
-#' @format A data frame with twelve variables:  \code{fake_student_id},
-#'  \code{school_year}, \code{gender}, \code{race}, \code{race_descr}, \code{migrant_ind},
-#'  \code{homeless_ind}, \code{sped_ind}, \code{sped_code}, \code{sped_descr},
-#'  \code{low_income_ind}, and \code{ell_status}.
 "dems_single_row_year"
 
 #' test_multi_row
@@ -36,11 +24,6 @@
 #' Demographic data of students. One student per row of data.
 #' Dataset has 500 students and 14 variables.
 #'
-#' @format A data frame with fourteen variables:  \code{school_year},
-#' \code{fake_student_id}, \code{fake_school_id}, \code{test_name},
-#' \code{test_period}, \code{opportunity_number}, \code{test_grade},
-#' \code{test_content_area}, \code{test_start_date}, \code{test_end_date},
-#' \code{test_sem}, \code{test_scale_score}, \code{test_performance_level}, and \code{test_raw_score}.
 "test_multi_row"
 
 #' test_single_row
@@ -48,11 +31,6 @@
 #' Demographic data of students. One student per row of data.
 #' Dataset has 8 students and 14 variables.
 #'
-#' @format A data frame with fourteen variables:  \code{school_year},
-#' \code{fake_student_id}, \code{fake_school_id}, \code{test_name},
-#' \code{test_period}, \code{opportunity_number}, \code{test_grade},
-#' \code{test_content_area}, \code{test_start_date}, \code{test_end_date},
-#' \code{test_sem}, \code{test_scale_score}, \code{test_performance_level}, and \code{test_raw_score}.
 "test_single_row"
 
 #' data_ggplot2
@@ -60,10 +38,6 @@
 #' Demographic and test data of students. One student per row of data.
 #' Dataset has 40,559 students and 8 variables.
 #'
-#' @format A data frame with eight variables:  \code{student_id},
-#' \code{school_id}, \code{tested_grade}, \code{math_scale_score_2013},
-#' \code{read_scale_score_2013}, \code{d_gender_m}, \code{d_econdisadv_y},
-#' and \code{d_lep_y}.
 "data_ggplot2"
 
 #' answer_2subset
