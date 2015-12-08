2b2e5be82864f324b8b7c43109068f35005c1fc5
zrussek <zrussek@EA10.EA.local>
2015-12-08 12:59:07 -0600

    package documentation

---
+++ R/pkgname.R
@@ -0,0 +1,28 @@
+
+
+#' Data package for training code.
+#'
+#' This package contains data that can be used with the training code.
+#' The names of the data included are bellow.
+#'
+#' @seealso \code{\link{answer_2subset}}
+#' @seealso \code{\link{answer_3merge}}
+#' @seealso \code{\link{answer_4recode}}
+#' @seealso \code{\link{answer_5operations}}
+#' @seealso \code{\link{answer_6bygroup}}
+#' @seealso \code{\link{answer_7transpose}}
+#' @seealso \code{\link{answer_9loop}}
+#' @seealso \code{\link{data_ggplot2}}
+#' @seealso \code{\link{dems_single_row_student}}
+#' @seealso \code{\link{dems_single_row_student_wgrade}}
+#' @seealso \code{\link{dems_single_row_year}}
+#' @seealso \code{\link{test_multi_row}}
+#' @seealso \code{\link{test_single_row}}
+#'
+#' @docType package
+#' @name DelawareDataPackage
+NULL
+
+
+
+
