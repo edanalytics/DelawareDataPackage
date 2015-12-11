c373db1b4270c7a8733617550953feea038fb447
zrussek <zrussek@EA18.EA.local>
2015-12-11 10:18:12 -0600

    change data in package to data.table from data.frame

--- data_raw/save_data.R
+++ data_raw/save_data.R
@@ -1,21 +1,36 @@
-dems_single_row_student <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_student.csv")
+library(data.table)
+
+# create dems_single_row_student
+dems_single_row_student <- fread("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_student.csv",
+                                 colClasses="character")
 save(dems_single_row_student, file = "data/dems_single_row_student.rdata")
 
-dems_single_row_student_wgrade <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_student_with_grade.csv")
+# create dems_single_row_student_wgrade
+dems_single_row_student_wgrade <- fread("P:/GitHub/DelawareDataPackage/data_raw/
+                                           deid_dems_single_row_per_student_with_grade.csv", stringsAsFactors = FALSE)
 save(dems_single_row_student_wgrade, file = "data/dems_single_row_student_wgrade.rdata")
 
-dems_single_row_year <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_year.csv")
+# create dems_single_row_year
+dems_single_row_year <- fread("P:/GitHub/DelawareDataPackage/data_raw/deid_dems_single_row_per_year.csv",
+                                 colClasses="character")
 save(dems_single_row_year, file = "data/dems_single_row_year.rdata")
 
-test_multi_row <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_test_multi_row.csv")
+# create test_multi_row
+test_multi_row <- fread("P:/GitHub/DelawareDataPackage/data_raw/deid_test_multi_row.csv",
+                        stringsAsFactors=FALSE,colClasses="character")
 save(test_multi_row, file = "data/test_multi_row.rdata")
 
-test_single_row <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/deid_test_single_row.csv")
+# create test_single_row
+test_single_row <- fread("P:/GitHub/DelawareDataPackage/data_raw/deid_test_single_row.csv",
+                            colClasses="character")
 save(test_single_row, file = "data/test_single_row.rdata")
 
-data_ggplot2 <- read.csv("P:/GitHub/DelawareDataPackage/data_raw/sample_test_data_ggplot2.csv")
+# create data_ggplot2
+data_ggplot2 <- fread("P:/GitHub/DelawareDataPackage/data_raw/sample_test_data_ggplot2.csv",
+                         colClasses = "character")
 save(data_ggplot2, file = "data/data_ggplot2.rdata")
 
+# create files for do-it-yourself sections
 doit_yourself <- load("P:/GitHub/DelawareDataPackage/data_raw/answers_doit_yourself.Rdata")
 for(i in 1:length(doit_yourself)){
   save(list = (doit_yourself[i]),
